13415c5ef8b5da6c465614fb14ab979cbbe949b0
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2022-12-03 13:06:36 +0300

    Models: Add User

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your models here. Okay.
+
+
+class User(AbstractUser):
+	username = models.CharField("Короткая ссылка", max_length=255, unique=True)
+	email = models.CharField("Email", max_length=255, unique=True)
+	password = models.CharField("Пароль", max_length=255)
+	fio = models.CharField("ФИО", max_length=255)
+	phone = models.CharField("Номер телефона", max_length=12) # TODO: change type from str to PhoneNumber or smth idk.
+
+	USERNAME_FIELD = 'username'
+
+	def __str__(self):
+		return str(self.username)
--- creative/settings.py
+++ creative/settings.py
@@ -123,4 +123,12 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'api.User'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
