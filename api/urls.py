281d2422585b2f08c1342a62fa926d154e530079
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2022-12-03 13:31:43 +0300

    API: Get user information by id

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 	
 	path('register', views.RegisterView.as_view(), name="registration"),
 	path('login', views.LoginView.as_view(), name="login"),
-	path('logout', views.LogoutView.as_view(), name="logout")
-
+	path('logout', views.LogoutView.as_view(), name="logout"),
+	path('user/get', views.UserGetView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -7,12 +7,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 
-
 from api.models import User
 
 
 def welcome(request):
 	return HttpResponse("<h1>Welcome</h1>")
+
 
 class RegisterView(APIView):
 	def post(self, request) -> Response:
@@ -20,6 +20,7 @@ class RegisterView(APIView):
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
 		return Response(serializer.data)
+
 
 class LoginView(APIView):
 	def post(self, request) -> Response:
@@ -32,7 +33,7 @@ class LoginView(APIView):
 		username = request.data["username"]
 		password = request.data["password"]
 		
-		user = User.objects.filter(username = username).first()
+		user = User.objects.filter(username=username).first()
 		
 		if user is None:
 			raise AuthenticationFailed("User not found!")
@@ -41,26 +42,50 @@ class LoginView(APIView):
 			raise AuthenticationFailed("Incorrect password!")
 		payload = {
 			"id": user.id,
-			"exp": datetime.datetime.utcnow() + datetime.timedelta(days = 60),
+			"exp": datetime.datetime.utcnow() + datetime.timedelta(days=60),
 			"iat": datetime.datetime.utcnow()
 		}
-
-		token = jwt.encode(payload, "secret", algorithm = "HS256")
-
+		
+		token = jwt.encode(payload, "secret", algorithm="HS256")
+		
 		response = Response()
-
-		response.set_cookie(key = "jwt", value = token, httponly = True)
+		
+		response.set_cookie(key="jwt", value=token, httponly=True)
 		response.data = {
 			"jwt": token
 		}
 		return response
 
+
 class LogoutView(APIView):
 	def post(self, request):
 		response = Response()
-
+		
 		response.delete_cookie("jwt")
 		response.data = {
 			"message": "success"
 		}
 		return response
+
+
+class UserGetView(APIView):
+	def get(self, request):
+		
+		token = request.COOKIES.get("jwt")
+		
+		if not token:
+			raise AuthenticationFailed("Unauthenticated!")
+		
+		try:
+			payload = jwt.decode(token, "secret", algorithms=["HS256"])
+		except jwt.ExpiredSignatureError:
+			raise AuthenticationFailed("Unauthenticated!")
+		
+		if not request.data.get("id"):
+			return Response(
+				{"id": "required"}
+			)
+		
+		user = User.objects.get(id=request.data["id"])
+		serializer = serializers.UserSerializer(user)
+		return Response(serializer.data)
