f0099e012e7a05f233a7bd3374078ff2c1234829
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2022-12-03 13:20:51 +0300

    API: Add login and JWT tokens

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
 	path('', views.welcome, name = 'welcome'),
 	path('register', views.RegisterView.as_view(), name = 'registration'),
+	path('login', views.LoginView.as_view(), name = "login")
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 
 
+from api.models import User
+
 
 def welcome(request):
 	return HttpResponse("<h1>Welcome</h1>")
@@ -18,3 +20,37 @@ class RegisterView(APIView):
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
 		return Response(serializer.data)
+
+class LoginView(APIView):
+	def post(self, request) -> Response:
+		if not request.data.get("username") or not request.data.get("password"):
+			raise AuthenticationFailed({
+				"username": "username",
+				"password": "password"
+			})
+		
+		username = request.data["username"]
+		password = request.data["password"]
+		
+		user = User.objects.filter(username = username).first()
+		
+		if user is None:
+			raise AuthenticationFailed("User not found!")
+		
+		if not user.check_password(password):
+			raise AuthenticationFailed("Incorrect password!")
+		payload = {
+			"id": user.id,
+			"exp": datetime.datetime.utcnow() + datetime.timedelta(days = 60),
+			"iat": datetime.datetime.utcnow()
+		}
+
+		token = jwt.encode(payload, "secret", algorithm = "HS256")
+
+		response = Response()
+
+		response.set_cookie(key = "jwt", value = token, httponly = True)
+		response.data = {
+			"jwt": token
+		}
+		return response
