1575ca4dd301863fe1aa78daa3c807f51a10c3e2
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2022-12-04 13:09:27 +0300

    API: Add comment leave

--- api/urls.py
+++ api/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
 	path("materials/get_from_user", views.GetFromUserMaterialsView.as_view()),
 	path("materials/create", views.CreateMaterialView.as_view()),
 	path("materials/comment_list", views.MaterialCommentList.as_view()),
+	path("materials/comment_leave", views.LeaveMaterialCommentView.as_view()),
 	
 	path('lenta/subscription', views.LentaSubscriptionView.as_view()),
 	path('likes/set', views.SetLikeMaterialView.as_view()),
